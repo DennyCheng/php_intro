793226b8dd001998a4319dc47bef12ab67aade14
dennycheng <chen2368@umn.edu>
2016-10-20 23:24:09 -0500

    variable tests

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <h1>PHP MAGIC!</h1>
+  </head>
+  <body>
+    <p>
+      <?php
+        echo"This is a test";
+        echo"Concatenation"."test";
+        echo 2 * 2;
+      ?>
+    </p>
+    <p>
+      <?php
+      $variableName = "Denny";
+      $myAge = 24;
+      echo "$variableName".""."age is"."$myAge";
+      ?>
+    </p>
+
+  </body>
+</html>
